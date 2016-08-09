2a22f5843cbd533e31c156ca2cd7ca0bae9b868d
Piotr Rybałtowski <piotrek@owl-labs.com>
2016-08-09 23:02:38 +0200

    start, end of day, month

--- src/DateTime.php
+++ src/DateTime.php
@@ -15,4 +15,48 @@ class DateTime extends \DateTimeImmutable
     {
         return new self();
     }
+
+    /**
+     * @return bool|DateTime
+     */
+    public function startOfDay()
+    {
+        return $this->setTime(0, 0, 0);
+    }
+
+    /**
+     * @return bool|DateTime
+     */
+    public function endOfDay()
+    {
+        return $this->setTime(23, 59, 59);
+    }
+
+    /**
+     * @param bool $startOfDay
+     * @return bool|DateTime
+     */
+    public function startOfMonth($startOfDay = true)
+    {
+        if ($startOfDay) {
+            $date = $this->startOfDay();
+        } else {
+            $date = $this;
+        }
+        return $date->setDate($date->format('Y'), $date->format('m'), 1);
+    }
+
+    /**
+     * @param bool $endOfDay
+     * @return bool|DateTime
+     */
+    public function endOfMonth($endOfDay = true)
+    {
+        if ($endOfDay) {
+            $date = $this->endOfDay();
+        } else {
+            $date = $this;
+        }
+        return $date->setDate($date->format('Y'), $date->format('m'), $date->format('t'));
+    }
 }
--- test/DateTimeTest.php
+++ test/DateTimeTest.php
@@ -14,4 +14,76 @@ class DateTimeTest extends \PHPUnit_Framework_TestCase
     {
         self::assertInstanceOf(DateTime::class, DateTime::now());
     }
+
+    public function testStartOfDay()
+    {
+        $date = new DateTime();
+        $expected = $date->format('Y-m-d H:i:s');
+        $date2 = $date->startOfDay();
+
+        self::assertNotSame($date2, $date);
+        self::assertEquals($date->format('Y-m-d'), $date2->format('Y-m-d'));
+        self::assertEquals($expected, $date->format('Y-m-d H:i:s'));
+        self::assertEquals('00:00:00', $date2->format('H:i:s'));
+    }
+
+    public function testEndOfDay()
+    {
+        $date = new DateTime();
+        $expected = $date->format('Y-m-d H:i:s');
+        $date2 = $date->endOfDay();
+
+        self::assertNotSame($date2, $date);
+        self::assertEquals($date->format('Y-m-d'), $date2->format('Y-m-d'));
+        self::assertEquals($expected, $date->format('Y-m-d H:i:s'));
+        self::assertEquals('23:59:59', $date2->format('H:i:s'));
+    }
+
+    public function testStartOfMonthWithStartOfDay()
+    {
+        $date = new DateTime();
+        $expected = $date->format('Y-m-d H:i:s');
+        $expected2 = $date->format('Y-m-01 00:00:00');
+        $date2 = $date->startOfMonth(true);
+
+        self::assertNotSame($date2, $date);
+        self::assertEquals($expected, $date->format('Y-m-d H:i:s'));
+        self::assertEquals($expected2, $date2->format('Y-m-d H:i:s'));
+    }
+
+    public function testStartOfMonthWithoutStartOfDay()
+    {
+        $date = new DateTime();
+        $expected = $date->format('Y-m-d H:i:s');
+        $expected2 = $date->format('Y-m-01 H:i:s');
+        $date2 = $date->startOfMonth(false);
+
+        self::assertNotSame($date2, $date);
+        self::assertEquals($expected, $date->format('Y-m-d H:i:s'));
+        self::assertEquals($expected2, $date2->format('Y-m-d H:i:s'));
+    }
+
+    public function testEndOfMonthWithEndOfDay()
+    {
+        $date = new DateTime();
+        $expected = $date->format('Y-m-d H:i:s');
+        $expected2 = $date->format('Y-m-t 23:59:59');
+        $date2 = $date->endOfMonth(true);
+
+        self::assertNotSame($date2, $date);
+        self::assertEquals($expected, $date->format('Y-m-d H:i:s'));
+        self::assertEquals($expected2, $date2->format('Y-m-d H:i:s'));
+    }
+
+    public function testEndOfMonthWithoutEndOfDay()
+    {
+        $date = new DateTime();
+        $expected = $date->format('Y-m-d H:i:s');
+        $expected2 = $date->format('Y-m-t H:i:s');
+        $date2 = $date->endOfMonth(false);
+
+        self::assertNotSame($date2, $date);
+        self::assertEquals($expected, $date->format('Y-m-d H:i:s'));
+        self::assertEquals($expected2, $date2->format('Y-m-d H:i:s'));
+    }
 }
