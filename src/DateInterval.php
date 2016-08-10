58c1e06e4abd475e1e9ffa197a01d75f13d9a605
X <x>
2016-08-10 10:26:10 +0200

    Start, end of week, year Yesterday, tomorrow Weekday, weekend NO TESTS!

---
+++ src/DateInterval.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace PiotrekR\DateTime;
+
+/**
+ * Class DateInterval
+ * @package PiotrekR\DateTime
+ */
+class DateInterval extends \DateInterval
+{
+}
---
+++ src/DateTimeImmutable.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace PiotrekR\DateTime;
+
+/**
+ * Class DateTimeImmutable
+ * @package PiotrekR\DateTime
+ */
+class DateTimeImmutable extends DateTime
+{
+}
